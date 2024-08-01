c10cefdadae3903c856615d21b5994cea8a6eab7
Hrshit5616 <2022aspire12@gmail.com>
2024-08-01 18:39:18 +0530

    new for deployment

--- backend/index.js
+++ backend/index.js
@@ -12,22 +12,19 @@ dotenv.config()
 const app=express()
 app.use(cors());
 app.use(cookieParser());
-// parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 app.use('/api',router);
 
-const __dirname=path.resolve();
+
 const PORT=3000 || process.env.PORT
 db().then(()=>{
     app.listen(PORT,()=>{
         console.log("server is running");
     })
 })
-app.use(express.static(path.join(__dirname,'/client/dist')));
-app.get('*',(req,res)=>{
-res.sendFile(path.join(__dirname,'client','dist','index.html'));
-})
+
 
