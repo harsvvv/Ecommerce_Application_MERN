1c9127bd3e54993b0957475da4a2344e88c2bfd0
Hrshit5616 <2022aspire12@gmail.com>
2024-08-28 21:27:27 +0530

    same side none added in backend

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -1,40 +1,52 @@
 import { userModel } from "../../models/userModel.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-export const userSignIN=async(req,res)=>{
+export const userSignIN = async (req, res) => {
     try {
-        const {password,email}=req.body;
-        if(!email){
-            throw new Error("please provide email");
+        const { password, email } = req.body;
+
+        if (!email) {
+            throw new Error("Please provide email");
         }
-        if(!password){
-            throw new Error("please provide password");
+        if (!password) {
+            throw new Error("Please provide password");
         }
-        const user=await userModel.findOne({email});
-        if(!user){
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
             throw new Error("User not found");
         }
-        const compare=bcryptjs.compareSync(password,user.password);
-        if(compare){
-            const token=await jwt.sign({id:user._id},process.env.JWT_SECRET);
-            res.cookie("token",token,{httpOnly:true}).status(200).json({
-                "message":"user loggin successfully",
-                statuscode:200,
+
+        const compare = bcryptjs.compareSync(password, user.password);
+
+        if (compare) {
+            const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: true,        // Ensure this is true when serving over HTTPS
+                sameSite: 'None',    // Cross-site cookie setup
+                maxAge: 24 * 60 * 60 * 1000  // Optional: Set cookie expiration time (24 hours here)
+            })
+            .status(200)
+            .json({
+                message: "User logged in successfully",
+                statusCode: 200,
                 user
-                })
+            });
 
-        }else{
-            throw new Error("wrong password");
+        } else {
+            throw new Error("Wrong password");
         }
-
 
     } catch (error) {
         console.log(error);
         res.json({
-            message:error.message,
-            error:true,
-            success:false
-        })
+            message: error.message,
+            error: true,
+            success: false
+        });
     }
-}
+};
